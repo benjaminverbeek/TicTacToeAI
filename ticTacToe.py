a1f3729ef424d6de7db55c9413cf7e205f9c17ad
Benjamin Verbeek <benjaminverbeek79@gmail.com>
2021-07-15 13:15:38 +0200

    remains: NN part

--- ticTacToe.py
+++ ticTacToe.py
@@ -24,6 +24,7 @@ class Board:
         return out[:-1] + "\n"
 
     def move(self, pos) -> None:
+        assert(pos in list(range(1,self.sz**2+1)))
         pos = pos-1 # list offset
         assert(self.state[pos] not in self.markers)  # if spot is taken, raise error
         self.state[pos] = self.decode[self.turn % 2 + 1] # automatic turn calculator
@@ -36,8 +37,10 @@ class Board:
         # Horizontal
         for row in range(sz):
             first = state[sz*row] # first element in row
+            if first not in self.markers:
+                continue
             for col in range(sz):   # column
-                if state[sz*row + col] != first or first not in ['X', 'O']:
+                if state[sz*row + col] != first:
                     same = False
                     break
                 else:
@@ -48,8 +51,10 @@ class Board:
         # Vertical
         for col in range(sz):
             first = state[col] # first element in col
+            if first not in self.markers:
+                continue
             for row in range(sz):   # rows
-                if state[sz*row + col] != first or first not in ['X', 'O']:
+                if state[sz*row + col] != first:
                     same = False
                     break
                 else:
@@ -58,10 +63,13 @@ class Board:
                 return True
 
         # Diagonal \
+        first = state[0] # first element on diag
         for diag in range(sz):
             row, col = diag, diag
-            first = state[sz*row + col] # first element in diag
-            if state[sz*row + col] != first or first not in ['X', 'O']:
+            if first not in self.markers:
+                same = False
+                break
+            if state[sz*row + col] != first:
                 same = False
                 break
             else:
@@ -70,10 +78,13 @@ class Board:
             return True
 
         # Diagonal /
+        first = state[sz-1] # first element in diag
         for diag in range(sz):
             row, col = diag, diag
-            first = state[sz*row + col] # first element in diag
-            if state[sz*(row+1) - (col+1)] != first or first not in ['X', 'O']:
+            if first not in self.markers:
+                same = False
+                break
+            if state[sz*(row+1) - (col+1)] != first:
                 same = False
                 break
             else:
@@ -87,27 +98,27 @@ def ticTacToe():
     b = Board(sz, disp=True)
     print("Welcome to Tic-Tac-Toe!")
     print(b)
-    print("Encoding is 1-9 top-left to bottom right. Quit anytime by entering '0'.")
+    print("Encoding is 1-9 top-left to bottom right. Quit anytime by entering 'q'.")
 
     while b.turn < 9:   # full board
         print(f"{b.decode[b.turn%2 + 1]}'s turn")
-        m = int(input("Enter a position to place: "))
-        if m == 0:
+        m = input("Enter a position to place: ")
+        if m == 'q':
             break
         try:
-            b.move(m)
+            b.move(int(m))
         except:
             print("Invalid move, try again!")
             continue
         if b.win() == True:
             print(f"{b.decode[(b.turn-1)%2 + 1]} won!")
             break
-    if b.turn == 9:
-        print("It's a draw.")
-    #print("Final state:")
-    #print(b)
+        elif b.turn == 9:
+            print("It's a draw.")
     print("Thank you for playing!")
 
 
 # TESTS
 ticTacToe()
+while input("Play again? Type 'y' for yes: ") == "y":
+    ticTacToe()
